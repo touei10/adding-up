1be752fffe1f734fee3af887e45b5cc4241f6f89
touei10 <tokoroten122121@gmail.com>
2019-06-25 11:46:44 +0900

    集計されたdataを格納する連想配列. dataの計算

--- app.js
+++ app.js
@@ -5,19 +5,36 @@ const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
 
-//  key: 都道府県、、 value: 集計データのobject
-// const prefectureDataMap = new Map();  
+const prefecturesDataMap = new Map();      //  key: 都道府県   value: 集計データのオブジェクト
 
 rl.on('line', (lineString) => {
     const columns = lineString.split(',');
     const year = parseInt(columns[0]);
-    const prefecture = columns[1];
+    const prefectures = columns[1];
     const popu = parseInt(columns[3]);
-    
+
     if (year === 2010 || year === 2015) {
-        console.log(year);
-        console.log(prefecture);
-        console.log(popu);
+        let value = prefecturesDataMap.get(prefectures);
+        if (!value) {
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if (year === 2010) {
+            value.popu10 = popu;
+        }
+        if (year === 2015) {
+            value.popu15 = popu;
+        }
+        prefecturesDataMap.set(prefectures, value);
     }
 
+});
+rl.on('close', () => {
+    for (let [key, value] of prefecturesDataMap) {
+        value.change = value.popu15 / value.popu10;
+    }
+    console.log(prefecturesDataMap);
 });
